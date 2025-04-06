850a2ab26a581684a1547e5253d65a8deba1ce3e
pluto <mackasto2005@gmail.com>
2025-04-06 15:30:13 +0200

    z41.cpp

--- z41.cpp
+++ z41.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 struct jazol{
-    int pod;
-    jazol *sled;
+    int pod;//podatok
+    jazol *sled;//sleden jazol
 };
 
 struct list{
     jazol *head;
     void init(){head=nullptr;}
     void kreiraj(int n){
+        head=new jazol;
         head->pod=n;
         head->sled=nullptr;
     }
@@ -20,8 +21,10 @@ struct list{
         head=p;
     }
     void brishiNapred(){
-        if(head!=nullptr){//go napraiv bolje 😙
-            /*if(head->sled==nullptr)head=head->sled;
+        if(head!=nullptr){//ne mora da se proveri dali head->sled==nullptr zasho ako e samo se stava head
+            //da e nullptr
+            /*vaka e vo prez:
+            if(head->sled==nullptr)head=head->sled;
             else{
                 jazol *p=head;
                 head=head->sled;
@@ -32,19 +35,119 @@ struct list{
            delete p;
         }
     }
+    void vnesiNazad(int n){
+        if(head!=nullptr){
+            jazol *p=head;
+            while(p->sled!=nullptr)p=p->sled;
+            p->sled=new jazol;
+            p=p->sled;
+            p->pod=n;
+            p->sled=nullptr;
+        }else{//ova mi e za da nemoram kreiraj koga e prazna listata
+            head=new jazol;
+            head->pod=n;
+            head->sled=nullptr;
+        }
+    }
+    void brishiPozadi(){
+        if(head!=nullptr){
+            if(head->sled==nullptr){
+                delete head;
+                head=nullptr;
+            }else{
+                jazol *p=head;
+                while(p->sled->sled!=nullptr)p=p->sled;
+                delete p->sled;
+                p->sled=nullptr; 
+            }
+        }else cout<<"Nema clenovi"<<endl;
+    }
+    void brishi(){
+        while(head!=nullptr)brishiPozadi();
+        cout<<"Izbrishano"<<endl;
+    }
+    void vnesiPred(jazol *j,int n){
+        if(head!=nullptr && j!=head){
+            jazol *p=new jazol,*b=head;
+            p->pod=n;
+            while(b!=nullptr && b->sled!=j)b=b->sled;
+            if(b!=nullptr){
+                p->sled=j;
+                b->sled=p;
+            }
+        }else cout<<"ne";
+    }
     void pechati(){
         for(jazol *p=head;p!=nullptr;p=p->sled)cout<<p->pod<<" ";
+        cout<<endl;
+    }
+    int len(){
+        jazol *p=head;
+        int n=0;
+        while(p!=nullptr){
+            p=p->sled;
+            n++;
+        }
+        return n;
     }
 };
 
+void brishiIsti(list &a,list &b){
+    int na=a.len(),nb=b.len();
+    jazol *pa=a.head,*pb=b.head,*pretha=nullptr,*prethb=nullptr;
+    bool pomesteno=false;
+    for(int i=0;i<(na>nb?nb:na);i++){//ternarniot e za da se najde koja e pomala lista i tolku pati da
+        //izvrti for ciklustot pr. ako imame listi so 4 i 5 clena for vrti 4 pati
+        jazol *ba,*bb;//jazli shto gi brisham
+        if(pa->pod==pb->pod && pretha!=nullptr){
+            ba=pa;
+            bb=pb;
+            pretha->sled=pa->sled;//prethodniot na clenot shot e ist vo a da pokazuva na clenot posle toj
+            //shto e ist so toj vo lista b
+            prethb->sled=pb->sled;
+            delete ba,bb;
+            pomesteno=true;
+        }else if(pa->pod==pb->pod){//ako nema prethoden toa znaci prvite se isti i se pomestuva head
+            ba=pa;
+            bb=bb;
+            a.head=a.head->sled;
+            b.head=b.head->sled;
+            delete ba,bb;
+        }else pomesteno=false;
+        if(pomesteno){//ako sme izbrishale nema potreba prethodnite da se pomestat samo pa i pb treba
+            pa=pa->sled;
+            pb=pb->sled;
+        }else{
+            pretha=pa;
+            prethb=pb;
+            pa=pa->sled;
+            pb=pb->sled;
+        }
+    }
+}
+
 int main(){
-    int n;
-    list a;
+    int n,na,nb;
+    list a,b;
     a.init();
-    a.vnesiNapred(1);
+    b.init();
+
+    cout<<"Dolzhina na l1 "<<endl;
+    cin>>na;
+    for(int i=0;i<na;i++){// so for ciklusi zasho ne mi raboteshe da prestane so char kako vo preth 
+        //zadaci 😭
+        cin>>n;
+        a.vnesiNazad(n);
+    }
+    cout<<"Dolzh na l2 "<<endl;
+    cin>>nb;
+    for(int i=0;i<nb;i++){
+        cin>>n;
+        b.vnesiNazad(n);
+    }
     a.pechati();
-    cout<<endl;
-    a.brishiNapred();
-    if(a.head==nullptr)cout<<"freak"<<endl;
+    b.pechati();
+    brishiIsti(a,b);
     a.pechati();
+    b.pechati();
 }
